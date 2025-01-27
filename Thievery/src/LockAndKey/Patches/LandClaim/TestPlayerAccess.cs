8f9ab5681554f106a12c95315c95cc15e47c3589
Chronolegionnaire <bryantbakeraz@gmail.com>
2025-01-26 21:25:14 -0700

    Lockable Blocks are exempt from land claim, keys can be reset by putting into grid.

---
+++ Thievery/src/LockAndKey/Patches/LandClaim/TestPlayerAccess.cs
@@ -0,0 +1,21 @@
+﻿using System.Linq;
+using HarmonyLib;
+using Vintagestory.API.Common;
+
+[HarmonyPatch(typeof(LandClaim), "TestPlayerAccess")]
+public class Patch_LandClaim_TestPlayerAccess
+{
+    static bool Prefix(IPlayer player, EnumBlockAccessFlags claimFlag, ref EnumPlayerAccessResult __result, LandClaim __instance)
+    {
+        var world = player.Entity.World;
+        var pos = player.Entity.Pos.AsBlockPos;
+        var block = world.BlockAccessor.GetBlock(pos);
+        if (block?.CollectibleBehaviors != null && 
+            block.CollectibleBehaviors.Any(b => b.GetType().Name == "BlockBehaviorLockable"))
+        {
+            __result = EnumPlayerAccessResult.OkOwner;
+            return false;
+        }
+        return true;
+    }
+}
